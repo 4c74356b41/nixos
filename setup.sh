064c5dc8285934629cab19831a22d30a4ac3219b
Gleb Boushev <spam@4c74356b41.com>
2026-08-01 12:44:55 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,43 +16,8 @@ flatpak override --user --filesystem=home com.visualstudio.code
 flatpak override --user --talk-name=org.freedesktop.Flatpak com.visualstudio.code
 flatpak override --user --env=DOCKER_HOST=unix:///run/user/1000/podman/podman.sock com.visualstudio.code
 flatpak override --user --filesystem=xdg-run/podman com.visualstudio.code
-flatpak override --user --socket=podman com.visualstudio.code
 flatpak override --user --filesystem=/run/user/$(id -u)/podman/podman.sock com.visualstudio.code
+flatpak override --user --socket=podman com.visualstudio.code
+systemctl --user enable --now podman.socket
 
-podman build -t runner:local .
-podman run --volume /home/sway/_git:/_git:Z -d runner:local --name agent
-git init --bare ~/.dotfiles
-dg remote add origin https://github.com/4c74356b41/dotfiles.git
 mkdir downloads
-rm -rf Desktop/ Documents/ Downloads/ Music/ Pictures/ Public/ Templates/ Videos/
-xdg-user-dirs-update
-systemctl --user mask xdg-user-dirs.service
-
-podman volume create onedrive_conf
-podman volume create onedrive_data
-export ONEDRIVE_DATA_DIR="${HOME}/od"
-export ONEDRIVE_UID=id -u
-export ONEDRIVE_GID=id -g
-mkdir -p ${ONEDRIVE_DATA_DIR}
-
-podman run -it --name onedrive -v onedrive_conf:/onedrive/conf \
-    --security-opt label=disable \
-    -v "${ONEDRIVE_DATA_DIR}:/onedrive/data" \
-    -e "ONEDRIVE_UID=${ONEDRIVE_UID}" \
-    -e "ONEDRIVE_GID=${ONEDRIVE_GID}" \
-    driveone/onedrive:edge
-
-chown 525287:525287 /home/sway/od/docs/pwd.kdbx
-sudo nano /var/home/sway/.local/share/containers/storage/volumes/onedrive_conf/_data/config
-# need to do "y" in terminal if doing resync
-# https://github.com/abraunegg/onedrive/blob/master/docs/docker.md#editing-the-running-configuration-and-using-a-config-file
-
-sudo setfacl -R -d -m u:sway:rwx ~/od
-sudo setfacl -R -d -m u:525287:rwx ~/od
-sudo setfacl -R -m u:525287:rwx ~/od
-sudo setfacl -R -m u:sway:rwx ~/od
-
-sudo setfacl -R -d -m u:sway:rwx ~/_git
-sudo setfacl -R -d -m u:525287:rwx ~/_git
-sudo setfacl -R -m u:525287:rwx ~/_git
-sudo setfacl -R -m u:sway:rwx ~/_git
