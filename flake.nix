a92fef2e5ff3d39dcc7cbbd292d7d9629f8f2297
Gleb Boushev <spam@4c74356b41.com>
2026-08-02 20:39:55 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/a2e55e31d6b0c104e92922c241ec5090be99b912";
+    nixpkgs.url = "github:NixOS/nixpkgs/148bab9c1c3c53136ecb44a6ea356a0ed5b39b06";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
