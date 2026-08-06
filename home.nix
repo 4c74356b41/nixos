910313136bd92dee341d6e75472368e324f41fc9
Gleb Boushev <spam@4c74356b41.com>
2026-08-06 06:53:41 +0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -120,6 +120,7 @@
           "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05+";
           "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 0.05-";
           "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          "XF86AudioPlay" = "exec playerctl play-pause";
           "${mod}+Tab" = "workspace next";
           "${mod}+v" = "exec copyq show";
           "${mod}+d" = "exec ${menuCommand}";
